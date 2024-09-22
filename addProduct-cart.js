a470797ef954de5f29da1395ebf85182483525e3
Le Quang Trong Nghia <lqtnghia2602@gmail.com>
2024-09-22 17:24:30 +0700

    fix showInfoProduct page and add list product

--- addProduct-cart.js
+++ addProduct-cart.js
@@ -1,5 +1,6 @@
 const products = [
   {
+    id: 0,
     img: "./img/img-hot-product/Mặt Nạ Đất Sét Beplain Từ Đậu Xanh Hỗ Trợ Thải Độc Da Giảm Mụn Đầu Đen Mung Bean Pore Clay Mask 120ml.webp",
     brand: "BEPLAIN",
     name: "Mặt Nạ Đất Sét Beplain Từ Đậu Xanh Hỗ Trợ Thải Độc Da Giảm Mụn Đầu Đen Mung Bean Pore Clay Mask 120ml",
@@ -7,6 +8,7 @@ const products = [
     rating: 5,
   },
   {
+    id: 1,
     img: "./img/img-hot-product/Phấn Nước Clio Kiềm Dầu, Lâu Trôi Kill Cover Skin Fixer Cushion 15g.webp",
     brand: "CLIO",
     name: "Phấn Nước Clio Kiềm Dầu, Lâu Trôi Kill Cover Skin Fixer Cushion 15g",
@@ -14,56 +16,66 @@ const products = [
     rating: 4,
   },
   {
+    id: 2,
     img: "./img/img-hot-product/Son Thỏi Bóng Peripera Heart Jam Glow Lip 1.4g.webp",
-    brand: "CLIO",
+    brand: "BEPLAIN",
     name: "Son Thỏi Bóng Peripera Heart Jam Glow Lip 1.4g",
-    price: 320000,
-    rating: 4,
+    price: 330000,
+    rating: 5,
   },
   {
+    id: 3,
     img: "./img/img-hot-product/Sáp Tẩy Trang Thuần Chay Banila Co Cho Da Lỗ Chân Lông To Clean it Zero Pore Clarifying Cleansing Balm.webp",
-    brand: "CLIO",
+    brand: "BEPLAIN",
     name: "Sáp Tẩy Trang Thuần Chay Banila Co Cho Da Lỗ Chân Lông To Clean it Zero Pore Clarifying Cleansing Balm",
-    price: 420000,
+    price: 295000,
     rating: 3,
   },
   {
-    img: "./img/img-hot-product/Phấn Nước Clio Kiềm Dầu, Lâu Trôi Kill Cover Skin Fixer Cushion 15g.webp",
-    brand: "CLIO",
-    name: "Phấn Nước Clio Kiềm Dầu, Lâu Trôi Kill Cover Skin Fixer Cushion 15g",
-    price: 420000,
+    id: 4,
+    img: "./img/img-hot-product/Mặt Nạ Đất Sét Beplain Từ Đậu Xanh Hỗ Trợ Thải Độc Da Giảm Mụn Đầu Đen Mung Bean Pore Clay Mask 120ml.webp",
+    brand: "BEPLAIN",
+    name: "Mặt Nạ Đất Sét Beplain Từ Đậu Xanh Hỗ Trợ Thải Độc Da Giảm Mụn Đầu Đen Mung Bean Pore Clay Mask 120ml",
+    price: 600000,
+    rating: 5,
+  },
+  {
+    id: 5,
+    img: "./img/img-hot-product/Mặt Nạ Đất Sét Beplain Từ Đậu Xanh Hỗ Trợ Thải Độc Da Giảm Mụn Đầu Đen Mung Bean Pore Clay Mask 120ml.webp",
+    brand: "BEPLAIN",
+    name: "Mặt Nạ Đất Sét Beplain Từ Đậu Xanh Hỗ Trợ Thải Độc Da Giảm Mụn Đầu Đen Mung Bean Pore Clay Mask 120ml",
+    price: 600000,
     rating: 4,
   },
   {
-    img: "./img/img-hot-product/Phấn Nước Clio Kiềm Dầu, Lâu Trôi Kill Cover Skin Fixer Cushion 15g.webp",
-    brand: "CLIO",
-    name: "Phấn Nước Clio Kiềm Dầu, Lâu Trôi Kill Cover Skin Fixer Cushion 15g",
-    price: 420000,
-    rating: 4,
+    id: 6,
+    img: "./img/img-hot-product/Mặt Nạ Đất Sét Beplain Từ Đậu Xanh Hỗ Trợ Thải Độc Da Giảm Mụn Đầu Đen Mung Bean Pore Clay Mask 120ml.webp",
+    brand: "BEPLAIN",
+    name: "Mặt Nạ Đất Sét Beplain Từ Đậu Xanh Hỗ Trợ Thải Độc Da Giảm Mụn Đầu Đen Mung Bean Pore Clay Mask 120ml",
+    price: 600000,
+    rating: 5,
   },
-  {
-    img: "./img/img-hot-product/Phấn Nước Clio Kiềm Dầu, Lâu Trôi Kill Cover Skin Fixer Cushion 15g.webp",
-    brand: "CLIO",
-    name: "Phấn Nước Clio Kiềm Dầu, Lâu Trôi Kill Cover Skin Fixer Cushion 15g",
-    price: 420000,
-    rating: 4,
-  },
-  // Thêm các sản phẩm khác
+  // Thêm các sản phẩm khác...
 ];
+
+// Lưu danh sách sản phẩm vào localStorage
+localStorage.setItem("products", JSON.stringify(products));
 
 const productContainer = document.querySelector(".list-hot-product");
 
 products.forEach((product) => {
   const productHTML = `
     <div class="hot-product">
-      <a href="#">
+      <a href="info-product.html?id=${product.id}">
         <div class="img-hot-product">
           <img src="${product.img}" />
         </div>
         <div class="detail-hot-product">
           <h6>${product.brand}</h6>
           <span>${product.name}</span>
-          <div class="price"><span>${product.price.toLocaleString()}<sup>đ</sup></span></div>
+          <div class="price">
+            <span>${product.price.toLocaleString()}<sup>đ</sup></span>
+          </div>
           <div>
             <ul class="star-rating ${product.rating}-star">
               ${'<li><a href="#">&#9733;</a></li>'.repeat(product.rating)}
@@ -76,5 +88,3 @@ products.forEach((product) => {
   `;
   productContainer.innerHTML += productHTML;
 });
-
-attachAddToCartEvents();
--- cart.js
+++ cart.js
@@ -59,12 +59,62 @@ function cartTotal() {
       .replace(/\./g, "")
       .replace(/\s/g, "")
       .replace("đ", "");
+
     totalA = inputValue * parseFloat(productPrice);
     totalC += totalA;
   }
 
   var cartTotalA = document.querySelector(".price-total span");
   cartTotalA.innerHTML = totalC.toLocaleString("de-DE") + "<sup>đ</sup>";
+}
+
+// Hàm thêm sự kiện thay đổi số lượng cho các ô nhập
+function updateCartQuantity() {
+  const quantityInputs = document.querySelectorAll(
+    "tbody tr input[type='number']"
+  );
+
+  quantityInputs.forEach(function (input) {
+    input.addEventListener("change", function () {
+      if (input.value < 1) {
+        input.value = 1; // Đảm bảo số lượng tối thiểu là 1
+      }
+      cartTotal(); // Cập nhật tổng tiền khi thay đổi số lượng
+    });
+  });
+}
+
+function addcart(productPrice, productImg, productName) {
+  productPrice = productPrice
+    .replace(/\./g, "")
+    .replace(/\s/g, "")
+    .replace("đ", "");
+  var addtr = document.createElement("tr");
+  var cartItem = document.querySelectorAll("tbody tr");
+
+  for (var i = 0; i < cartItem.length; i++) {
+    var productT = document.querySelectorAll(".title-name");
+    if (productT[i].innerHTML == productName) {
+      alert("Sản phẩm của bạn đã có trong giỏ hàng.");
+      return;
+    }
+  }
+
+  var trcontent =
+    '<tr><td style="display: flex; align-items: center"><img style="width: 70px" src="' +
+    productImg +
+    '"/><span class="title-name">' +
+    productName +
+    '</span></td><td><p><span class="title-price">' +
+    productPrice +
+    '</span><sup>đ</sup></p></td><td><input style="width: 30px; outline: none" type="number" value="1" min="1" /></td><td style="cursor: pointer"><span class="title-delete">Xóa</span></td></tr>';
+  addtr.innerHTML = trcontent;
+  var cartTable = document.querySelector("tbody");
+  cartTable.append(addtr);
+
+  cartTotal();
+  deleteCart();
+  updateCartQuantity(); // Gọi hàm để cập nhật sự kiện sau khi thêm sản phẩm
 }
 
 function deleteCart() {
