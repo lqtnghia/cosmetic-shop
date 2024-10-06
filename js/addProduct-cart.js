693dc14feca341e2e3419c8b763bfd801990a551
Le Quang Trong Nghia <lqtnghia2602@gmail.com>
2024-10-06 22:19:22 +0700

    build page show and filter all products

--- js/addProduct-cart.js
+++ js/addProduct-cart.js
@@ -2,119 +2,145 @@ const products = [
   {
     id: 1,
     img: "../assets/img/img-hot-product/AHC/Bộ Sản Phẩm Chống Lão Hóa Da AHC Youthful Skin Starter Kit (4Pc).webp",
+    feature: "New",
     brand: "AHC",
     name: "Bộ Sản Phẩm Chống Lão Hóa Da AHC Youthful Skin Starter Kit (4Pc)",
     price: 420000,
     rating: 4,
     count: 26,
+    sale: 20,
   },
   {
     id: 2,
     img: "../assets/img/img-hot-product/AHC/Kem Dưỡng Da AHC Ampoule Directory PHA Mattifying Gel Cream 30ml.webp",
+    feature: "New",
     brand: "AHC",
     name: "Kem Dưỡng Da AHC Ampoule Directory PHA Mattifying Gel Cream 30ml.webp",
     price: 330000,
     rating: 5,
     count: 3,
+    sale: 30,
   },
   {
     id: 3,
     img: "../assets/img/img-hot-product/AHC/Tinh Chất AHC Dưỡng Chuyên Sâu Dạng Viên Youth Focus Intensive Night Capsule Serum (30 capsules).webp",
+    feature: "New",
     brand: "AHC",
     name: "Tinh Chất AHC Dưỡng Chuyên Sâu Dạng Viên Youth Focus Intensive Night Capsule Serum (30 capsules)",
     price: 295000,
     rating: 3,
     count: 4,
+    sale: 20,
   },
   {
     id: 4,
     img: "../assets/img/img-hot-product/BEPLAIN/Mặt Nạ Đất Sét Beplain Từ Đậu Xanh Hỗ Trợ Thải Độc Da Giảm Mụn Đầu Đen Mung Bean Pore Clay Mask 120ml.webp",
+    feature: "New",
     brand: "BEPLAIN",
     name: "Mặt Nạ Đất Sét Beplain Từ Đậu Xanh Hỗ Trợ Thải Độc Da Giảm Mụn Đầu Đen Mung Bean Pore Clay Mask 120ml",
     price: 600000,
     rating: 5,
     count: 5,
+    sale: 0,
   },
   {
     id: 5,
     img: "../assets/img/img-hot-product/BANILA CO/(Phiên bản Vegan) Sáp Tẩy Trang Thuần Chay Banila Co Cho Da Khô Clean it Zero Nourishing Cleansing Balm 100ml.webp",
+    feature: "New",
     brand: "BANILA CO",
     name: "(Phiên bản Vegan) Sáp Tẩy Trang Thuần Chay Banila Co Cho Da Khô Clean it Zero Nourishing Cleansing Balm 100ml",
     price: 600000,
     rating: 4,
     count: 6,
+    sale: 0,
   },
   {
     id: 6,
     img: "../assets/img/img-hot-product/BANILA CO/(Phiên bản Vegan) Sáp Tẩy Trang Thuần Chay Banila Co Cho Da Nhạy Cảm Clean it Zero Calming Cleansing Balm 100ml.webp",
+    feature: "New",
     brand: "BANILA CO",
     name: "(Phiên bản Vegan) Sáp Tẩy Trang Thuần Chay Banila Co Cho Da Nhạy Cảm Clean it Zero Calming Cleansing Balm 100ml",
     price: 600000,
     rating: 5,
     count: 16,
+    sale: 20,
   },
   {
     id: 7,
     img: "../assets/img/img-hot-product/BANILA CO/(Phiên bản Vegan) Sáp Tẩy Trang Thuần Chay Banila Co Cho Mọi Loại Da Clean it Zero Original Cleansing Balm.webp",
+    feature: "New",
     brand: "BANILA CO",
     name: "(Phiên bản Vegan) Sáp Tẩy Trang Thuần Chay Banila Co Cho Mọi Loại Da Clean it Zero Original Cleansing Balm",
     price: 600000,
     rating: 5,
     count: 29,
+    sale: 0,
   },
   {
     id: 8,
     img: "../assets/img/img-hot-product/BANILA CO/Sáp Tẩy Trang Thuần Chay Banila Co Cho Da Lỗ Chân Lông To Clean it Zero Pore Clarifying Cleansing Balm.webp",
+    feature: "New",
     brand: "BANILA CO",
     name: "Sáp Tẩy Trang Thuần Chay Banila Co Cho Da Lỗ Chân Lông To Clean it Zero Pore Clarifying Cleansing Balm",
     price: 600000,
     rating: 5,
     count: 30,
+    sale: 0,
   },
   {
     id: 9,
     img: "../assets/img/img-hot-product/CLUB CLIO/Kem Nền Bóng Mượt Clio Kill Cover Glow Foundation Spf50+ Pa++++ 38G.webp",
+    feature: "New",
     brand: "CLUB CLIO",
     name: "Kem Nền Bóng Mượt Clio Kill Cover Glow Foundation Spf50+ Pa++++",
     price: 600000,
     rating: 5,
     count: 67,
+    sale: 0,
   },
   {
     id: 10,
     img: "../assets/img/img-hot-product/CLUB CLIO/Kem Nền Che Khuyết Điểm Clio Kill Cover Founwear Foundation SPF30 PA+++ 38g.webp",
+    feature: "New",
     brand: "CLUB CLIO",
     name: "Kem Nền Che Khuyết Điểm Clio Kill Cover Founwear Foundation SPF30 PA+++",
     price: 600000,
     rating: 5,
     count: 55,
+    sale: 0,
   },
   {
     id: 11,
     img: "../assets/img/img-hot-product/CLUB CLIO/Kem Nền Căng Mướt Da Dear Dahlia Skin Paradise Sheer Silk Foundation 30Ml.webp",
+    feature: "New",
     brand: "CLUB CLIO",
     name: "Kem Nền Căng Mướt Da Dear Dahlia Skin Paradise Sheer Silk Foundation 30Ml",
     price: 600000,
     rating: 5,
     count: 99,
+    sale: 10,
   },
   {
     id: 12,
     img: "../assets/img/img-hot-product/DEAR DAHLIA/Bảng Màu Mắt 4 Ô Dear Dahlia Dream Velvet Eyeshadow Palette 5.1g.webp",
+    feature: "New",
     brand: "DEAR DAHLIA",
     name: "Bảng Màu Mắt 4 Ô Dear Dahlia Dream Velvet Eyeshadow Palette 5.1g",
     price: 600000,
     rating: 5,
     count: 12,
+    sale: 15,
   },
   {
     id: 13,
     img: "../assets/img/img-hot-product/DEAR DAHLIA/Son Thỏi Hiệu Ứng Lì Mịn Dear Dahlia Lip Paradise Effortless Matte Lipstick (3,2G).webp",
+    feature: "New",
     brand: "DEAR DAHLIA",
     name: "Son Thỏi Hiệu Ứng Lì Mịn Dear Dahlia Lip Paradise Effortless Matte Lipstick (3,2G) ",
     price: 600000,
     rating: 5,
     count: 34,
+    sale: 0,
   },
   // Thêm các sản phẩm khác...
 ];
@@ -123,46 +149,82 @@ const products = [
 localStorage.setItem("products", JSON.stringify(products));
 
 //Lọc sản phẩm bán chạy (10 sản phẩm có số lượng bạn lớn nhất)
-
-const productContainer = document.querySelector(".list-hot-product");
 const listProductHot = products.sort((a, b) => b.count - a.count).slice(0, 10);
-
+// Các sản phẩm bán chạy
+const productContainer = document.querySelector(".list-hot-product");
 listProductHot.forEach((product) => {
-  const productHTML = `
-    <div class="product">
-      <a href="info-product.html?id=${product.id}">
-        <div class="img-product">
-          <img src="${product.img}" />
-          <button class="show-now">Xem ngay</button>
-        </div>
-        <div class="detail-product">
-          <h6>${product.brand}</h6>
-          <span>${product.name}</span>
-          <div class="price">
-            <span>${product.price.toLocaleString()}<sup>đ</sup></span>
+  let productHotHTML = "";
+  if (product.sale > 0) {
+    // Nếu sản phẩm có giảm giá
+    productHotHTML = `
+      <div class="product">
+        <a href="info-product.html?id=${product.id}">
+          <div class="feature">${product.sale}%</div>
+          <div class="img-product">
+            <img src="${product.img}" />
+            <button class="show-now">Xem ngay</button>
           </div>
-          <div>
-            <ul class="star-rating ${product.rating}-star">
-              ${'<li><a href="#">&#9733;</a></li>'.repeat(product.rating)}
-              <span class="product-count">(${product.count})</span>
-            </ul>
+          <div class="detail-product">
+            <h6>${product.brand}</h6>
+            <span>${product.name}</span>
+            <div class="price">
+              <span class="old-price" style="text-decoration: line-through;">${product.price.toLocaleString()}<sup>đ</sup></span>
+              <span class="official-price">${(
+                product.price -
+                (product.price * product.sale) / 100
+              ).toLocaleString()}<sup>đ</sup></span>
+            </div>
+            <div>
+              <ul class="star-rating ${product.rating}-star">
+                ${'<li><a href="#">&#9733;</a></li>'.repeat(product.rating)}
+                <span class="product-count">(${product.count})</span>
+              </ul>
+            </div>
           </div>
+          <button class="addcart">Thêm vào giỏ hàng</button>
+        </a>
+      </div>
+    `;
+  } else {
+    // Các sản phẩm còn lại
+    productHotHTML = `
+        <div class="product">
+          <a href="info-product.html?id=${product.id}">
+            <div class="feature">${product.feature}</div>
+            <div class="img-product">
+              <img src="${product.img}" />
+              <button class="show-now">Xem ngay</button>
+            </div>
+            <div class="detail-product">
+              <h6>${product.brand}</h6>
+              <span>${product.name}</span>
+              <div class="price">
+                <span class="old-price">${product.price.toLocaleString()}<sup>đ</sup></span>
+              </div>
+              <div>
+                <ul class="star-rating ${product.rating}-star">
+                  ${'<li><a href="#">&#9733;</a></li>'.repeat(product.rating)}
+                  <span class="product-count">(${product.count})</span>
+                </ul>
+              </div>
+            </div>
+            <button class="addcart">Thêm vào giỏ hàng</button>
+          </a>
         </div>
-        <button class="addcart">Thêm vào giỏ hàng</button>
-      </a>
-    </div>
-  `;
-  productContainer.innerHTML += productHTML;
+      `;
+  }
+  // // Gán HTML sản phẩm vào container
+  productContainer.innerHTML += productHotHTML;
 });
 
 //  lọc sản phẩm sale
-const listSaleProduct = products.filter((product) => product.id > 7);
-
+const listSaleProduct = products.filter((product) => product.sale > 0);
 const productSale = document.querySelector(".list-sale-product");
 listSaleProduct.forEach((product) => {
-  const productHTML = `
+  const productSaleHTML = `
     <div class="product">
       <a href="info-product.html?id=${product.id}">
+      <div class="feature">${product.sale}%</div>
         <div class="img-product">
           <img src="${product.img}" />
           <button class="show-now">Xem ngay</button>
@@ -171,7 +233,11 @@ listSaleProduct.forEach((product) => {
           <h6>${product.brand}</h6>
           <span>${product.name}</span>
           <div class="price">
-            <span>${product.price.toLocaleString()}<sup>đ</sup></span>
+            <span class="old-price" style=" text-decoration: line-through;">${product.price.toLocaleString()}<sup>đ</sup></span>
+            <span class="official-price">${(
+              product.price -
+              (product.price * product.sale) / 100
+            ).toLocaleString()}<sup>đ</sup></span>
           </div>
           <div>
             <ul class="star-rating ${product.rating}-star">
@@ -184,5 +250,5 @@ listSaleProduct.forEach((product) => {
       </a>
     </div>
   `;
-  productSale.innerHTML += productHTML;
+  productSale.innerHTML += productSaleHTML;
 });
--- js/cart.js
+++ js/cart.js
@@ -8,44 +8,14 @@ function attachAddToCartEvents() {
       var product = btnItem.closest(".product");
       var productImg = product.querySelector("img").src;
       var productName = product.querySelector("span").innerText;
-      var productPrice = product.querySelector(".price span").innerText;
+      var productPrice = product.querySelector(".price .offical-price")
+        ? product.querySelector(".price .official-price").innerText
+        : product.querySelector(".price .old-price").innerText;
 
       addcart(productPrice, productImg, productName);
     });
   });
 }
-
-// function addcart(productPrice, productImg, productName) {
-//   productPrice = productPrice
-//     .replace(/\./g, "")
-//     .replace(/\s/g, "")
-//     .replace("đ", "");
-//   var addtr = document.createElement("tr");
-//   var cartItem = document.querySelectorAll("tbody tr");
-
-//   for (var i = 0; i < cartItem.length; i++) {
-//     var productT = document.querySelectorAll(".title-name");
-//     if (productT[i].innerHTML == productName) {
-//       alert("Sản phẩm của bạn đã có trong giỏ hàng.");
-//       return;
-//     }
-//   }
-
-//   var trcontent =
-//     '<tr><td style="display: flex; align-items: center"><img style="width: 70px" src="' +
-//     productImg +
-//     '"/><span class="title-name">' +
-//     productName +
-//     '</span></td><td><p><span class="title-price">' +
-//     productPrice +
-//     '</span><sup>đ</sup></p></td><td><input style="width: 30px; outline: none" type="number" value="1" min="1" /></td><td style="cursor: pointer"><span class="title-delete">Xóa</span></td></tr>';
-//   addtr.innerHTML = trcontent;
-//   var cartTable = document.querySelector("tbody");
-//   cartTable.append(addtr);
-
-//   cartTotal();
-//   deleteCart();
-// }
 
 function cartTotal() {
   var cartItem = document.querySelectorAll("tbody tr");
@@ -99,7 +69,6 @@ function addcart(productPrice, productImg, productName) {
   cartTotal();
   deleteCart();
   updateCartQuantity(); // Gọi hàm để cập nhật sự kiện sau khi thêm sản phẩm
-  updateCartForInfoProduct();
 }
 
 function deleteCart() {
@@ -130,20 +99,6 @@ function updateCartQuantity() {
     });
   });
 }
-
-//Hàm thêm sự kiện thay đổi số lương cho ô nhập trong page info-product
-// function updateCartForInfoProduct() {
-//   const countChange = document.querySelectorAll(
-//     ".product .foot-detail input[type='number']"
-//   );
-//   countChange.addEventListener("change", function () {
-//     const valueNew = countChange.value;
-//   });
-//   const count = document.querySelector("form tbody input[type='number']");
-//   btn.addEventListener("click", function () {
-//     count.value = valueNew;
-//   });
-// }
 
 // Hiển thị/Ẩn giỏ hàng
 const cartbtn = document.querySelector(".fa-x");
